9d5ef2589220253c80e4b9016a8fe547280ba4e1
fod0rr <fod0rr.maria@gmail.com>
2025-09-12 14:33:42 -0300

    subir

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TP1;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace TP1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+	private void OnOkButtonClicked(object sender, EventArgs e)
+    {
+        if (IdEntry.Text == "admin" && PassEntry.Text == "senha@dmin")
+        {
+            DisplayAlert("Login", "Logou com sucesso", "OK");
+        }
+        else
+        {
+            DisplayAlert("Login", "Login não autorizado", "OK");
+        }
+    }
+
+	private void OnLimparButtonClicked(object sender, EventArgs e)
+    {
+        IdEntry.Text = string.Empty;
+        PassEntry.Text = string.Empty;
+
+        IdEntry.Focus();
+    }
+
+    /// <summary>
+    /// Evento disparado ao clicar no botão Créditos.
+    /// Exibe os nomes dos autores.
+    /// </summary> 
+	private async void OnCreditosButtonClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Créditos", "Desenvolvido por:\n\n- Maria Eduarda Fodor - CB3025063\n- Pedro Xavier Oliveira -  CB3027376", "Fechar");
+    }
+}
